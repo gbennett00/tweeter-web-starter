4eee80d1e4d739e7e1de4b8107ec1cd65b622aae
Garrett Bennett <bennett.g00@gmail.com>
2024-10-04 17:00:13 -0600

    Refactor PostStatus.tsx to use MVP design

---
+++ tweeter-web/src/presenters/PostStatusPresenter.ts
@@ -0,0 +1,47 @@
+import { AuthToken, Status, User } from "tweeter-shared";
+import { StatusService } from "../model/service/StatusService";
+
+export interface PostStatusView {
+  displayInfoMessage(message: string, duration: number): void;
+  displayErrorMessage(message: string): void;
+  clearLastInfoMessage(): void;
+  setIsLoading(isLoading: boolean): void;
+  setPost(post: string): void;
+}
+
+export class PostStatusPresenter {
+  private view: PostStatusView;
+  private statusService = new StatusService();
+
+  public constructor(view: PostStatusView) {
+      this.view = view;
+  }
+
+  public async submitPost(event: React.MouseEvent, post: string, authToken: AuthToken, currentUser: User) {
+    event.preventDefault();
+
+    try {
+      this.view.setIsLoading(true);
+      this.view.displayInfoMessage("Posting status...", 0);
+
+      const status = new Status(post, currentUser!, Date.now());
+
+      await this.statusService.postStatus(authToken!, status);
+
+      this.view.setPost("");
+      this.view.displayInfoMessage("Status posted!", 2000);
+    } catch (error) {
+      this.view.displayErrorMessage(
+        `Failed to post the status because of exception: ${error}`
+      );
+    } finally {
+      this.view.clearLastInfoMessage();
+      this.view.setIsLoading(false);
+    }
+  }
+
+  public clearPost(event: React.MouseEvent) {
+    event.preventDefault();
+    this.view.setPost("");
+  }
+}
