0fb2c9f6708321734863d78c8122a8645f29ef93
Garrett Bennett <bennett.g00@gmail.com>
2024-10-04 17:13:29 -0600

    Refactor UserNavigationHook.ts to use MVP design

--- tweeter-web/src/components/userInfo/UserNavigationHook.ts
+++ tweeter-web/src/components/userInfo/UserNavigationHook.ts
@@ -1,47 +1,20 @@
-import { AuthToken, FakeData, User } from "tweeter-shared";
 import useToastListener from "../toaster/ToastListenerHook";
 import useUserInfo from "./UserInfoHook";
+import { UserNavigationPresenter, UserNavigationView } from "../../presenters/UserNavigationPresenter";
+import { useState } from "react";
 
 const useUserNavigation = () => {
   const { setDisplayedUser, currentUser, authToken } = useUserInfo();
   const { displayErrorMessage } = useToastListener();
-  
-  const navigateToUser = async (event: React.MouseEvent): Promise<void> => {
-    event.preventDefault();
-  
-    try {
-      const alias = extractAlias(event.target.toString());
-  
-      const user = await getUser(authToken!, alias);
-  
-      if (!!user) {
-        if (currentUser!.equals(user)) {
-          setDisplayedUser(currentUser!);
-        } else {
-          setDisplayedUser(user);
-        }
-      }
-    } catch (error) {
-      displayErrorMessage(`Failed to get user because of exception: ${error}`);
-    }
-  };
-  
-  const extractAlias = (value: string): string => {
-    const index = value.indexOf("@");
-    return value.substring(index);
-  };
-  
-  const getUser = async (
-    authToken: AuthToken,
-    alias: string
-  ): Promise<User | null> => {
-    // TODO: Replace with the result of calling server
-    return FakeData.instance.findUserByAlias(alias);
-  };
 
-  return navigateToUser;
+  const listener: UserNavigationView = {
+    setDisplayedUser: setDisplayedUser,
+    displayErrorMessage: displayErrorMessage,
+  }
+
+  const [presenter] = useState(() => new UserNavigationPresenter(listener));
+
+  return (event: React.MouseEvent) => presenter.navigateToUser(event, authToken!, currentUser!);
 }
-
-
 
 export default useUserNavigation;
---
+++ tweeter-web/src/presenters/UserNavigationPresenter.ts
@@ -0,0 +1,47 @@
+import { AuthToken, User } from "tweeter-shared";
+import { UserService } from "../model/service/UserService";
+
+export interface UserNavigationView {
+  setDisplayedUser(user: User): void;
+  displayErrorMessage(message: string): void;
+}
+
+export class UserNavigationPresenter {
+  private view: UserNavigationView;
+  private userService = new UserService();
+
+  public constructor(view: UserNavigationView) {
+    this.view = view;
+  }
+
+  public async navigateToUser(
+    event: React.MouseEvent,
+    authToken: AuthToken,
+    currentUser: User
+  ): Promise<void> {
+    event.preventDefault();
+
+    try {
+      const alias = this.extractAlias(event.target.toString());
+
+      const user = await this.userService.getUser(authToken!, alias);
+
+      if (!!user) {
+        if (currentUser!.equals(user)) {
+          this.view.setDisplayedUser(currentUser!);
+        } else {
+          this.view.setDisplayedUser(user);
+        }
+      }
+    } catch (error) {
+      this.view.displayErrorMessage(
+        `Failed to get user because of exception: ${error}`
+      );
+    }
+  }
+
+  private extractAlias(value: string): string {
+    const index = value.indexOf("@");
+    return value.substring(index);
+  }
+}
