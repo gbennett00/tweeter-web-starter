e9e5e24612b3c25256aa76297f812f868ba41542
Garrett Bennett <bennett.g00@gmail.com>
2024-11-04 19:46:18 -0700

    First lambda

--- tweeter-shared/src/index.ts
+++ tweeter-shared/src/index.ts
@@ -7,3 +7,6 @@ export { AuthToken } from "./model/domain/AuthToken";
 // All classes that should be avaialble to other modules need to exported here. export * does not work when 
 // uploading to lambda. Instead we have to list each export.
 export { FakeData } from "./util/FakeData";
+export type { UserDto } from "./model/dto/UserDto";
+export type { PagedUserItemRequest } from "./model/net/request/PagedUserItemRequest";
+export type { PagedUserItemResponse } from "./model/net/response/PagedUserItemResponse.ts";
